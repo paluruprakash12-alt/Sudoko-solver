92652b149aa0e8aaa273651391013b1e6717aba9
paluruprakash12-alt <paluruprakash12@gmail.com>
2026-06-18 11:20:27 +0530

    Add files via upload

---
+++ Sudoku solver.py
@@ -0,0 +1,78 @@
+def print_board(board):
+    for row in board:
+        print(row)
+
+
+def find_empty(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                return row, col
+    return None
+
+
+def is_valid(board, num, pos):
+    row, col = pos
+
+    # Check row
+    for i in range(9):
+        if board[row][i] == num and i != col:
+            return False
+
+    # Check column
+    for i in range(9):
+        if board[i][col] == num and i != row:
+            return False
+
+    # Check 3x3 box
+    box_x = col // 3
+    box_y = row // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
+def solve(board):
+    empty = find_empty(board)
+
+    if not empty:
+        return True
+
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, num, (row, col)):
+            board[row][col] = num
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+print("Unsolved Sudoku:")
+print_board(board)
+
+if solve(board):
+    print("\nSolved Sudoku:")
+    print_board(board)
+else:
+    print("No solution exists.")
